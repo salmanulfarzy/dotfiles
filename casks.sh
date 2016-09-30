c3d8325df522af00bf96deb2143c7ff33afe1f68
Salmanul Farzy <salmanulfarzy@gmail.com>
2016-09-30 10:53:58 +0530

    cask: develoment tools

---
+++ casks.sh
@@ -0,0 +1,21 @@
+# Install native apps
+#
+# brew cask
+#
+#
+
+
+# Dev tools
+brew cask install iterm2
+brew cask install atom
+brew cask install android-studio
+brew cask install rstudio
+brew cask install mactex
+brew cask install sublime-text
+brew cask install virtualbox
+
+
+# Utilities
+brew cask install android-file-transfer
+brew cask install appcleaner
+
